2a7533bb0a106bccee9a284b6901aea2176075eb
glaxia <gustavo@laxiabot.com>
2022-06-29 02:30:40 -0500

    implementando firebase como base de datos

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,37 +1,65 @@
 const { httpError } = require('../helpers/handleError');
 const userModel = require('../models/users')
+const { db } = require('../../config/firebase')
 
 const getItems = async (req, res) => {
     try {
-        const listAll = await userModel.find({})
-        res.send({ data: listAll })
+        //const listAll = await userModel.find({}) //mongo
+        const querySnapshot = await db.collection('usuarios').get() //firebase
+        const usuarios = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+        res.send({ data: usuarios }) // cambiar querysnapshot por listAll si se usa mongo
     } catch (e) {
         httpError(res, e)
     }
 }
 
-const getItem = (req, res) => {
+const getItem = async (req, res) => {
+    try {
+        const doc = await db.collection('usuarios').doc(req.params.id).get()
 
+        res.send({ status: 'usuario obtenido', data: { id: doc.id, ...doc.data() } })
+    } catch (e) {
+        httpError(res, e)
+    }
 }
 
 const createItem = async (req, res) => {
     try {
-        const { name, age, email } = req.body
-        const resDetail = await userModel.create({
-            name, age, email
+        const { nombre, renovo, correo, contrasena } = req.body
+        //const resDetail = await userModel.create({ name, age, email })
+        await db.collection('usuarios').add({
+            nombre,
+            renovo,
+            correo,
+            contrasena
         })
-        res.send({ data: resDetail })
+        res.send({ data: 'usuario creado' })
     } catch (e) {
         httpError(res, e)
     }
 }
 
-const updateItem = (req, res) => {
+const updateItem = async (req, res) => {
+    try {
+        await db.collection('usuarios').doc(req.params.id).update(req.body)
 
+        res.send({ data: 'usuario editado' })
+    } catch (e) {
+        httpError(res, e)
+    }
 }
 
-const deleteItem = (req, res) => {
+const deleteItem = async (req, res) => {
+    try {
+        await db.collection('usuarios').doc(req.params.id).delete()
 
+        res.send({ data: 'usuario eliminado' })
+    } catch (e) {
+        httpError(res, e)
+    }
 }
 
 module.exports = { getItems, getItem, createItem, updateItem, deleteItem }
